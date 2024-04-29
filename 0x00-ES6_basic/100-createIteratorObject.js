b7a0b27612b8f9daac418beeb60dfc5222212c61
K.A. Osei-Sarfo <astrokojo@gmail.com>
2024-04-29 21:55:43 +0000

    lint compliant

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -2,15 +2,15 @@ const createIteratorObject = (report) => {
   let employees = [];
   for (const department of Object.values(report.allEmployees)) {
     employees = employees.concat(department);
-  }
+  };
 
   return {
-    *[Symbol.iterator]() {
-      for (const employee of employees) {
-        yield employee;
-      }
-    },
+      *[Symbol.iterator]() {
+        for (const employee of employees) {
+          yield employee;
+        };
+      },
   };
-};
+}
 
 export default createIteratorObject;
