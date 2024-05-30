be6f9cfaebc616ac9f8d7490d7f783de009f7e9f
K.A. Osei-Sarfo <astrokojo@gmail.com>
2024-05-30 02:44:55 +0000

    stle: add comments

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,5 +1,6 @@
 export default class Building {
   constructor(sqft) {
+    // Fix for throwing errors when ocerride rules are not enforced using the `new.target`. 
     if (new.target !== Building && this.evacuationWarningMessage === undefined) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
