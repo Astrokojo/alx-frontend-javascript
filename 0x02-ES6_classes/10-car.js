f3306b8fd1dfb8aa0b8c27cbe0c51f87281cf50c
K.A. Osei-Sarfo <astrokojo@gmail.com>
2024-05-30 23:40:30 +0000

    done task 10

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,14 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    if (![brand, motor, color].every((param) => typeof param === 'string')) {
+      throw new Error('all three parameters must be strings');
+    }
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    return new this.constructor(this._brand, this._color, this._motor);
+  }
+}
